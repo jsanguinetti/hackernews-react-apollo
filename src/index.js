4404faab2fc21a11ae068d5ad9d0b4ac6a16cc37
jsanguinetti <jsangui@gmail.com>
2017-08-26 12:52:54 -0300

    First version, connected to API, using ENV vars

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const networkInterface = createNetworkInterface({
+    uri: process.env.REACT_APP_API_ENDPOINT
+})
+
+networkInterface.use([{
+    applyMiddleware(req, next) {
+        if (!req.options.headers) {
+            req.options.headers = {};  // Create the header object if needed.
+        }
+        // get the authentication token from local storage if it exists
+        const token = process.env.REACT_APP_JWT_ACCESS_TOKEN;
+        req.options.headers.authorization = token ? `JWT ${token}` : null;
+        next();
+    }
+}])
+
+const client = new ApolloClient({
+    networkInterface
+})
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>, 
+    document.getElementById('root')
+)
 registerServiceWorker();
