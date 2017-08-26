58bf5c1d536ca24dd82a57f47e47afa169c6ca4f
jsanguinetti <jsangui@gmail.com>
2017-08-26 15:45:58 -0300

    Added a Login component. Using local storage for authentication, and defaulting to ENV var for the first token

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
+import { AUTH_TOKEN } from './constants'
 import { BrowserRouter } from 'react-router-dom'
 
 const networkInterface = createNetworkInterface({
@@ -14,7 +15,9 @@ networkInterface.use([{
     applyMiddleware(req, next) {
         req.options.headers = req.options.headers || {}
         // get the authentication token from local storage if it exists
-        const token = process.env.REACT_APP_JWT_ACCESS_TOKEN;
+        // All queries and mutations are authenticated in server, so we need at least some token
+        // Thats why I use a default token. (Didn't want other people messing around with data)
+        const token = localStorage.getItem(AUTH_TOKEN) || process.env.REACT_APP_JWT_ACCESS_TOKEN;
         req.options.headers.Authorization = token ? `JWT ${token}` : null;
         next();
     }
