acad136eb005f264b8f2f16550520c682e7e9e77
jsanguinetti <jsangui@gmail.com>
2017-09-02 17:38:39 -0300

    added voting and updating cache after mutations

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { graphql, gql } from 'react-apollo'
+import { ALL_LINKS_QUERY } from './LinkList'
 
 class CreateLink extends Component {
 
@@ -42,6 +43,14 @@ class CreateLink extends Component {
       variables: {
         description,
         url
+      },
+      update: (store, { data: { createLink } }) => {
+        const data = store.readQuery({ query: ALL_LINKS_QUERY })
+        data.allLinks.splice(0,0,createLink)
+        store.writeQuery({
+          query: ALL_LINKS_QUERY,
+          data
+        })
       }
     })
     this.props.history.push(`/`)
--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -18,23 +18,45 @@ class LinkList extends Component {
     return (
         <div>
             {
-                linksToRender.map(link => (
-                    <Link key={link.id} link={link}/>
+                linksToRender.map((link, index) => (
+                    <Link key={link.id} updateStoreAfterVote={this._updateCacheAfterVote} index={index} link={link}/>
                 ))
             }
         </div>
     )
   }
 
+    _updateCacheAfterVote = (store, createVote, linkId) => {
+        // 1
+        const data = store.readQuery({ query: ALL_LINKS_QUERY })
+
+        // 2
+        const votedLink = data.allLinks.find(link => link.id === linkId)
+        votedLink.votes = createVote.link.votes
+
+        // 3
+        store.writeQuery({ query: ALL_LINKS_QUERY, data })
+    }
+
 }
 
-const ALL_LINKS_QUERY = gql`
+
+export const ALL_LINKS_QUERY = gql`
 query AllLinksQuery {
-  allLinks {
-    id
-    url
-    description
-  }
+    allLinks {
+        id
+        url
+        description
+        postedBy {
+            name
+        }
+        votes {
+            id
+            user {
+                id
+            }
+        }
+    }
 }
 `
 
